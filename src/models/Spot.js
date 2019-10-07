70a51b6237f16a4ba36cd4e9304243f111e55770
EmanuelCampos <rebortgames@gmail.com>
2019-10-06 21:13:30 -0300

    simple change

--- src/models/Spot.js
+++ src/models/Spot.js
@@ -16,7 +16,7 @@ const SpotSchema = new mongoose.Schema({
     })
 
 SpotSchema.virtual('thumbnail_url').get(function () {
-    return `https://f-aircnc.herokuapp.com/${this.thumbnail}`
+    return `localhost:3333/files/${this.thumbnail}`
 })
 
 module.exports = mongoose.model('Spot', SpotSchema)
